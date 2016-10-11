064ddd1bd2734e41570759f85d5a0ae329d4400c
kpatel311 <kpatel311@gatech.edu>
2016-10-11 17:48:14 -0400

    Fixed Projects to support grouped and ungrouped students. Added some better retrieval functions

--- GroupUp/imports/collections/projects.js
+++ GroupUp/imports/collections/projects.js
@@ -10,13 +10,15 @@ Meteor.methods({
     Projects.insert({
       professor: data.professor,
       name: data.name,
+      link: data.link,
       createdAt: new Date(),
       description: data.description,
       deadline: data.deadline,
       min_teammates: data.min_teammates,
       max_teammates: data.max_teammates,
       skills: data.skills,
-      student_emails: data.student_emails,
+      ungrouped: data.student_emails,
+      groups: [],
     });
   },
 });
--- GroupUp/server/main.js
+++ GroupUp/server/main.js
@@ -15,9 +15,22 @@ Meteor.startup(() => {
       var parse = require('csv-parse/lib/sync');
       return parse(csvFile);
     },
+
+    'getGroupStats'(projectId) {
+      return null;
+    },
   });
 
   Meteor.publish('projects', function(limit) {
     return Projects.find({"professor": this.userId}, {'limit': limit});
   });
+
+  Meteor.publish('projectsById', function(projectId) {
+    return Projects.findOne({_id:projectId});
+  });
+
+  Meteor.publish("groupsInProject", function(projectId){
+    return Projects.findOne({_id: projectId}, {fields: Projects.groups});
+  });
+
 });
